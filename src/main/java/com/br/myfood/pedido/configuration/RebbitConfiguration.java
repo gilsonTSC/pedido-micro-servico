16b1855e01e42f5417d39da61397d2abe526c045
Gilson Trajano <gilson.costa@dce.ufpb.br>
2020-11-13 18:58:52 -0300

    Recebendo as mensagens do rebbitMQ da api cadastro

--- src/main/java/com/br/myfood/pedido/configuration/RebbitConfiguration.java
+++ src/main/java/com/br/myfood/pedido/configuration/RebbitConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -30,5 +31,10 @@ public class RebbitConfiguration {
 		rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
 		return rabbitTemplate;
 	}
+	
+	@Bean
+    public MessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 	
 }
